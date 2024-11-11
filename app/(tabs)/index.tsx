d267aa342334196beffedeec27e1592618085042
Maya Engel <mayaengelmusic@gmail.com>
2024-11-11 21:57:41 +0100

    added list generation of recipes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,87 @@
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, FlatList } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+const recipes = [{
+  id: 0,
+  name: 'pasta',
+  instructions: 'dosmth',
+}, {
+  id: 1,
+  name: 'potatoes',
+  instructions: 'dosmth',
+}, {
+  id: 2,
+  name: 'lasagne',
+  instructions: 'dosmth',
+}, {
+  id: 3,
+  name: 'grateng',
+  instructions: 'dosmth',  
+}, {
+  id: 4,
+  name: 'soup',
+  instructions: 'dosmth',
+}, {
+  id: 5,
+  name: 'pizza',
+  instructions: 'dosmth',  
+}, {
+  id: 7,
+  name: 'curry',
+  instructions: 'dosmth',  
+}, {
+  id: 8,
+  name: 'sdfoisdf',
+  instructions: 'dosmth',  
+}];
+
+
+
+export function GenerateNewMeal() {
+
+  return (
+    <Meal/>
+  );
+
+};
+
+export function Meal() {
+  return (
+    <>
+      <Text></Text>
+      <View style={styles.separator}/>
+    </>
+  );
+}
+
 export default function TabTwoScreen() {
+  const listItems = recipes.map(recipe =>
+    <li>{recipe.name}</li>
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Meal plan</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={styles.separator}/>
-      <Button title="New meal plan"/>
-      <View style={styles.separator}/>
       <Text style={styles.title}>This is the meal plan for this week</Text>
       <View style={styles.separator}/>
       
-      <Meal />
-      <Meal />
-      <Meal />
-      <Meal />
-      <Meal />
-      <Meal />
-      <Meal />
+      <Button title="New meal plan"/>
+      <View style={styles.separator}/>
 
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
-  );
-}
-
-export function Meal() {
-  return (
-    <>
-    <Text>This is a meal</Text>
-    <View style={styles.separator}/></>
   );
 }
 
--- app/(tabs)/recipes.tsx
+++ app/(tabs)/recipes.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Button } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
